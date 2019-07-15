3f87aa71cc533e5b42569196a046cf04d689d2ba
Thiago Soares Mota <thiagomota456@outlook.com>
2019-07-14 21:53:51 -0300

    Na próxima leio apenas a documentação e economizo dinheiro

---
+++ Biblioteca/MatematicaOperacao/Soma.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatematicaOperacao
+{
+    public class Soma
+    {
+        public static double OpSoma(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
